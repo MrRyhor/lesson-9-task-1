f0aa0de83059d1a4fb4d713c834d5bf2e2643b18
MrRyhor <grigory.karpyak@gmail.com>
2023-11-08 23:20:32 +0100

    first commit

---
+++ src/components/CompaniesFilter.vue
@@ -0,0 +1,78 @@
+<template>
+    <h1>Filters:</h1>
+    <div class="filter-container">
+        <label
+            >Name:
+            <input v-model="nameInp" type="text" />
+        </label>
+        <label
+            >Tax:
+            <input v-model="taxInp" type="number" />
+        </label>
+        <label
+            >Year:
+            <input v-model="yearInp" type="number" />
+        </label>
+        <label
+            >Owner:
+            <input v-model="ownerInp" type="text" />
+        </label>
+    </div>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+    name: 'CompaniesFilter',
+
+    computed: {
+        ...mapGetters(['getNameVal', 'getTaxVal', 'getYearVal', 'getOwnerVal']),
+        nameInp: {
+            get() {
+                return this.getNameVal
+            },
+            set(val) {
+                return this.setNameVal(val)
+            },
+        },
+        taxInp: {
+            get() {
+                return this.getTaxVal
+            },
+            set(val) {
+                return this.setTaxVal(val)
+            },
+        },
+        yearInp: {
+            get() {
+                return this.getYearVal
+            },
+            set(val) {
+                return this.setYearVal(val)
+            },
+        },
+        ownerInp: {
+            get() {
+                return this.getOwnerVal
+            },
+            set(val) {
+                return this.setOwnerVal(val)
+            },
+        },
+    },
+    methods: {
+        ...mapActions(['setNameVal', 'setTaxVal', 'setYearVal', 'setOwnerVal']),
+    },
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+    text-align: left;
+    padding-left: 50px;
+}
+.filter-container {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding-left: 50px;
+}
+</style>
---
+++ src/components/CompaniesManager.vue
@@ -0,0 +1,18 @@
+<template>
+    <companies-filter />
+    <companies-list :companies-data="getFilteredByOwnerCompaniesList" />
+</template>
+<script>
+import CompaniesList from './CompaniesList.vue'
+import CompaniesFilter from './CompaniesFilter.vue'
+import { mapGetters } from 'vuex'
+export default {
+    name: 'CompaniesManager',
+    components: { CompaniesList, CompaniesFilter },
+
+    computed: {
+        ...mapGetters(['getFilteredByOwnerCompaniesList']),
+    },
+}
+</script>
+<style lang="scss" scoped></style>
